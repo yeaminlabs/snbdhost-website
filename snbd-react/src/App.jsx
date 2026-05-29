8b45d74261eea01f00f462739496d1c7517653ac
Yeamin Adib <yeaminadib@Yeamins-MacBook-Air-2.local>
2026-05-28 19:38:15 -0700

    feat: redirect all 404/unmatched routes to the homepage

--- snbd-react/src/App.jsx
+++ snbd-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -112,6 +112,7 @@ function App() {
                   </ProtectedRoute>
                 }
               />
+              <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </main>
 
